60c6180606bad6f3bf73811a1117689acaa3de71
oquinena <daniel@oquinena.com>
2017-08-18 11:06:18 +0200

    minor updates

--- bin/toggle_touchpad.sh
+++ bin/toggle_touchpad.sh
@@ -8,7 +8,7 @@ status="$(xinput --list-props $device | grep "Device Enabled" | awk '{ print $4}
 if [ $status == "1" ]; then
 	xinput --set-prop $device "Device Enabled" 0
 	echo "Touchpad turned off"
-	#notify-send -a Touchpad "Touchpad turned off"
+	notify-send -a Touchpad "Touchpad turned off"
 else
 	xinput --set-prop $device "Device Enabled" 1
 	xinput --set-prop $device "libinput Tapping Enabled" 1&
--- bin/vpn-toggle.sh
+++ bin/vpn-toggle.sh
@@ -9,7 +9,7 @@ if [ $connection == "88775469" ] || [ $connection == "88775470" ]
  then
 	nmcli c up Ängkärr 
 	sudo ip route add 192.168.0.4 dev vpn0  
-elif [ $connection == "Angkarrskolan" ] 
+elif [ $connection == "Angkarrskolan" ] || [ $connection == "Angkarrskolan Ethernet" ]
  then
 	nmcli c up Hemma
 	sudo ip route add 192.168.2.2 dev ppp0
