ee2ef053360484a3dcefdff67a258e019bf00e46
daniel.oquinena <daniel@fast2.se>
2019-08-17 10:33:30 +0200

    minor changes

--- bin/toggle_touchpad.sh
+++ bin/toggle_touchpad.sh
@@ -9,11 +9,11 @@ status=$(xinput --list-props $device | grep "Device Enabled" | awk '{ print $4}'
 if [ $status == "1" ]; then
 	xinput --set-prop $device "Device Enabled" 0
     echo "Touchpad turned off"
-	notify-send -a Touchpad "Touchpad turned off"
+	#notify-send -a Touchpad "Touchpad turned off"
 else
 	echo "Touchpad turned on"
 	xinput --set-prop $device "Device Enabled" 1
 	xinput --set-prop $device "libinput Tapping Enabled" 1&
 	xinput --set-prop $device "libinput Natural Scrolling Enabled" 1
-	notify-send -a Touchpad "Touchpad turned on"
+	#notify-send -a Touchpad "Touchpad turned on"
 fi
